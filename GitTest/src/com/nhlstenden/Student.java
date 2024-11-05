91ad9a42d6ae08ec988f4b21498bb50ba86b4081
bochengpeng <bocheng.peng@student.nhlstenden.com>
2024-11-05 10:32:06 +0100

    update10.31

--- GitTest/src/com/nhlstenden/Student.java
+++ GitTest/src/com/nhlstenden/Student.java
@@ -1,3 +1,5 @@
+package com.nhlstenden;
+
 public class Student
 {
     private String name;
---
+++ GitTest/src/com/nhlstenden/StudentTest.java
@@ -0,0 +1,14 @@
+package com.nhlstenden;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest
+{
+
+    @Test
+    void setAge()
+    {
+    }
+}
